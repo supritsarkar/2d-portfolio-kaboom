7f4c16a36260cea5eefba726c820600e5ba1fff3
Suprit Sarkar <supritsarkar390@gmail.com>
2025-07-02 04:48:44 +0530

    Initial commit: 2D portfolio game with Kaboom.js

---
+++ src/constant.js
@@ -0,0 +1,35 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC setup — where I bring code to life.
+I mostly work in JavaScript and TypeScript, building web apps and games.
+From debugging to deploying, this desk has seen it all.`,
+
+  "bca-degree": `This is my BCA degree — a journey through theory, UNIX, DBMS, and a lot of practicals.
+It gave me the base, but real growth came through building and breaking things on my own.`,
+
+  desk: `This desk is where my projects take shape. From planning full-stack MERN apps to designing gameplay logic in Kaboom.js,
+it’s the battlefield where every bug gets squashed and every idea comes alive.`,
+
+  tv: `My screen for breaks. But let's be honest — I usually end up watching JavaScript tutorials,
+Next.js breakdowns, or someone building a game with pure JS. Productivity, but make it passive.`,
+
+  bed: `This is where rest happens — and ideas strike.
+I often lie down and end up mentally designing components or thinking through DSA problems.
+Sleep comes second when logic is looping in your head.`,
+
+  resume: `This is my desk and on it is my resume.Gonna upoload soon !`,
+
+//   projects: `This entire portfolio is built with Kaboom.js and Tiled map editor.
+// The player, map, and hitboxes are dynamically rendered. Movement and dialogue systems are handwritten.
+// Everything you see is powered by raw JavaScript and passion.`,
+
+  library: `My digital bookshelf isn't about books — it's about skills.
+Right now it's stacked with Next.js, React, TypeScript, MongoDB, and Java for DSA practice.
+I believe building projects is the best way to learn.`,
+
+  exit: `Want to exit my dev world? Just close the tab.
+But you’ll miss the rest of the story — I’m just getting started.`,
+
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+};
---
+++ src/kaboomCtx.js
@@ -0,0 +1,7 @@
+import kaboom from 'kaboom';
+export const k = kaboom({
+    global : false,
+    touchToMouse:true,//for mobile
+    canvas : document.getElementById("game")
+
+})
